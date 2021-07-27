95ed4254d7b3977ce04dff8fb329096d909f5da5
AlexVipond <alex.r.vipond@gmail.com>
2021-07-27 11:12:17 -0500

    setting up

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="h-screen w-screen flex items-center justify-center bg-gray-900">
+    <h1 class="text-2xl tracking-widest font-bold uppercase text-gray-400">Kenya Fertilizer Dashboard</h1>
+  </main>
+  <footer>
+    <p>Data source: <a href="https://africafertilizer.org">Africafertilizer.org</a></p>
+    <p><a href="https://vifaakenya.org/#/kenya/license">License</a></p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+  }
+})
+</script>
